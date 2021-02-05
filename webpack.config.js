447113b2fb3a355a3324c53f36d23d9391543c44
panferov54 <panferovm@gmail.com>
2021-02-05 15:20:37 +0700

    finish project config

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path=require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin=require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const isProd=process.env.NODE_ENV==='production'
+const isDev=!isProd
+
+//переменные которые показывают в каком мы режиме
+console.log('IS PROD', isProd)
+console.log('IS DEF', isDev)
+
+const filename=ext=>isDev?`bundle.${ext}`:`bundle.[hash],${ext}`
+
+module.exports={
+    context: path.resolve(__dirname,'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill','./index.js'],
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname,'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@':path.resolve(__dirname,'src'),
+            '@core':path.resolve(__dirname,'src/core')
+        }
+    },
+    devtool: isDev?'source-map':false,
+    devServer: {
+        port:4000,
+        hot:isDev
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: 'index.html',
+            minify: {
+                removeComments:isProd,
+                collapseWhitespace:isProd
+            }
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve(__dirname,'src/favicon.ico'),
+                    to: path.resolve(__dirname,'dist')},
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                        }
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }}
+        ],
+    }
+}
