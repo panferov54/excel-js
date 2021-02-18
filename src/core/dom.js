ce61a0cd3c63915b96d200b6db42711807af53ed
panferov54 <panferovm@gmail.com>
2021-02-18 11:52:44 +0700

    added selection group of cells with a mouse click

--- src/core/dom.js
+++ src/core/dom.js
@@ -69,6 +69,17 @@ constructor(selector) {
         })
     }
 
+    id(parse){
+    if(parse){
+        const parsed=this.id().split(':')
+        return {
+            row:+parsed[0],
+            col:+parsed[1]
+        }
+    }
+    return this.data.id
+    }
+
     addClass(className){
     this.$el.classList.add(className)
     }
